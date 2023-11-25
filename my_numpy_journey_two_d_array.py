8da766ef5f49b129a7a92e36c438e3e080aeed25
Judicaeld17e <judicaeldjidonou3@gmail.com>
2023-11-25 18:32:08 +0100

    first commit

---
+++ my_numpy_journey_two_d_array.py
@@ -0,0 +1,4 @@
+#TASK : Create a function which take a standard 2d array as parameter and returns a Numpy Array.
+import numpy as np 
+def my_numpy_journey_two_d_array(tab):
+    return np.array([tab])
